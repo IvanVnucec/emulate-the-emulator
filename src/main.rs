e5bda74eaa79381abe51d36942f8fb2bb779e30d
Ivan Vnucec <vnucec.ivan@gmail.com>
2023-05-21 15:29:15 +0200

    add move and and instr

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,51 @@
 #![allow(warnings)]
 
-#[derive(Debug)]
+use std::fmt;
+
+#[derive(Debug, Copy, Clone)]
 enum Instr {
-    Nop,
-    Jmp,
-    Ldr,
+    And(usize, usize, usize),
+    Mov(usize, u8),
 }
 
-impl From<u8> for Instr {
-    fn from(value: u8) -> Self {
-        match value {
-            0 => Instr::Nop,
-            1 => Instr::Jmp,
-            2 => Instr::Ldr,
-            _ => panic!("Invalid value for conversion to Instr"),
+impl From<u16> for Instr {
+    fn from(v: u16) -> Self {
+        let opcode = ((v >> 12) as u8) & 0xF;
+        let params = v & 0xFFF;
+
+        match opcode {
+            1 => {
+                let rs1 = (params & 0b11) as usize;
+                let rs2 = ((params >> 2) & 0b11) as usize;
+                let rd = ((params >> 4) & 0b11) as usize;
+                Instr::And(rd, rs1, rs2)
+            }
+            2 => {
+                let rd = ((params >> 8) & 0b11) as usize;
+                let val = params as u8;
+                Instr::Mov(rd, val)
+            }
+            _ => panic!("Unknown opcode: {opcode}"),
+        }
+    }
+}
+
+impl From<Instr> for u16 {
+    fn from(i: Instr) -> Self {
+        match i {
+            Instr::And(rd, rs1, rs2) => {
+                let opcode = 1_u16 << 12;
+                let params = (((rd as u16) & 0b11) << 4)
+                    | (((rs2 as u16) & 0b11) << 2)
+                    | ((rs1 as u16) & 0b11);
+
+                opcode | params
+            }
+            Instr::Mov(rd, val) => {
+                let opcode = 2_u16 << 12;
+                let params = (((rd as u16) & 0b11) << 8) | (val as u16);
+                opcode | params
+            }
         }
     }
 }
@@ -21,11 +53,9 @@ impl From<u8> for Instr {
 #[derive(Debug)]
 struct CPU<'a> {
     mem: &'a [u8],
-    pc: u8,
-    ir: u8,
-    r0: u8,
-    r1: u8,
-    r2: u8,
+    pc: u16,
+    ir: u16,
+    reg: [u8; 4],
 }
 
 impl<'a> CPU<'a> {
@@ -34,55 +64,76 @@ impl<'a> CPU<'a> {
             mem: mem,
             pc: 0,
             ir: 0,
-            r0: 0,
-            r1: 0,
-            r2: 0,
+            reg: [0; 4],
         }
     }
 
     fn tick(&mut self, repeat: usize) {
+        println!("{self}");
         for _ in 0..repeat {
             self.fetch();
-            let instr = self.decode();
+            let instr = self.ir.into();
             self.execute(instr);
+            println!("{self}");
         }
     }
 
     fn fetch(&mut self) {
-        self.ir = self.mem[self.pc as usize];
-        self.pc += 1;
-    }
-
-    fn decode(&self) -> Instr {
-        self.ir.into()
+        self.ir =
+            ((self.mem[self.pc as usize + 1] as u16) << 8) | (self.mem[self.pc as usize] as u16);
+        self.pc += 2;
     }
 
     fn execute(&mut self, instr: Instr) {
         println!("{:?}", instr);
 
         match instr {
-            Instr::Nop => (),
-            Instr::Jmp => {
-                self.pc = self.r0;
+            Instr::And(rd, rs1, rs2) => {
+                self.reg[rd] = self.reg[rs1] & self.reg[rs2];
             }
-            Instr::Ldr => {
-                self.r0 = self.mem[self.pc as usize + 2];
-                self.pc += 1;
-            }
+            Instr::Mov(rd, val) => self.reg[rd] = val,
+        }
+    }
+}
+
+impl<'a> fmt::Display for CPU<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "pc: {:>5?}, ir: 0x{:0>4X?}, regs: {:>3?}",
+            self.pc, self.ir, self.reg
+        )
+    }
+}
+
+struct Memory<const SIZE: usize> {
+    data: [u8; SIZE],
+}
+
+impl<const SIZE: usize> Memory<SIZE> {
+    fn new() -> Self {
+        Self { data: [0; SIZE] }
+    }
+
+    fn load_program(&mut self, program: Vec<Instr>) {
+        for (i, instr) in program.iter().enumerate() {
+            let instr_value = u16::from(*instr);
+            self.data[i * 2] = instr_value as u8;
+            self.data[i * 2 + 1] = (instr_value >> 8) as u8;
         }
     }
 }
 
 fn main() {
-    let mut mem: [u8; 1024] = [0; 1024];
-    mem[0..4].copy_from_slice(&[
-        // while true
-        Instr::Nop as u8,
-        Instr::Ldr as u8,
-        0,
-        Instr::Jmp as u8,
-    ]);
+    let mut mem = Memory::<1024>::new();
+    let prog = vec![
+        Instr::Mov(0, 0b11),
+        Instr::Mov(1, 0b01),
+        Instr::And(0, 1, 0),
+        Instr::And(2, 0, 0),
+    ];
 
-    let mut cpu = CPU::new(&mem);
+    mem.load_program(prog);
+    let mut cpu = CPU::new(&mem.data);
     cpu.tick(10);
 }
